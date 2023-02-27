3aedba3ba8efb4f8e583a293064efd82fe022cab
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-27 20:05:29 +0600

    mongo added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
@@ -13,7 +14,19 @@ app.use(cors());
 // port
 const PORT = process.env.PORT || 5000;
 
-// listening for requests
-app.listen(PORT, (req, res) => {
-  console.log(`listening on port ${PORT}`);
-});
+// connect to db
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // listening for requests
+    app.listen(PORT, (req, res) => {
+      console.log(`connected to db and listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
